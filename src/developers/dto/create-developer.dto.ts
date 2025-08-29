ca83d201ed3c4ea79253e194c587c338ad3a5eac
Guilherme Augusto <guilhermedev23@gmail.com>
2025-08-29 18:47:42 -0300

    ok

--- src/developers/dto/create-developer.dto.ts
+++ src/developers/dto/create-developer.dto.ts
@@ -1,13 +1,12 @@
 import { IsDateString, IsEmail, IsString } from "class-validator";
 
 export class CreateDeveloperDto {
-    @IsString()
-    name: string;
+  @IsString()
+  name: string;
 
-    @IsEmail()
-    email: string;
-    
-    @IsDateString()
-    dateOfBirth: string;
+  @IsEmail()
+  email: string;
 
-}
+  @IsDateString()
+  dateOfBirth: string;
+};
